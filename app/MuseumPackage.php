98cb9c90843f9f34d8e89cb64d074aa35aa736d9
KG <maclarenkung@gmail.com>
2020-01-28 16:39:59 +0700

    nullable create museum

--- app/MuseumPackage.php
+++ app/MuseumPackage.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class MuseumPackage extends Model
 {
+    protected $fillable = ['package_id', 'museum_id', 'expiry_date'];
+    protected $casts = [
+        'expiry_date'  => 'date:d - m - y',
+    ];
     public function package()
     {
         return $this->belongsTo('App\Package');
--- app/MuseumUser.php
+++ app/MuseumUser.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class MuseumUser extends Model
 {
-    //
+    protected $fillable = ['user_id', 'museum_id'];
 }
