408a122f223489baf3c0641b362fae631f1056fc
KG <maclarenkung@gmail.com>
2020-01-31 12:39:12 +0700

    floor create show

--- app/FloorTranslation.php
+++ app/FloorTranslation.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class FloorTranslation extends Model
 {
-    protected $fillable =  ['lang_id', 'item_id', 'name', 'description', 'audio_url'];
+    protected $fillable =  ['lang_id', 'floor_id', 'name', 'description', 'audio_url'];
 }
